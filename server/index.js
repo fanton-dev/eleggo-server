0297775983c11abb63217796200e1f906fb263a5
Maddie02 <madlensarkisian@gmail.com>
2021-03-12 19:56:50 +0200

    [Feat] Created post that starts process References #2.

--- server/index.js
+++ server/index.js
@@ -1,7 +1,23 @@
 const express = require('express');
+const cors = require('cors');
+const { spawn } = require('child_process');
 
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+
 const PORT = process.env.PORT || 5000;
+
+app.post('/enable-detection', (req, res) => {
+    console.log(req.body)
+    const { mode, code: {left, none, right, prerequisite} } = req.body;
+
+    spawn('python', [`${mode}.py\', ${left}, ${none}, ${right}, ${prerequisite}`]);
+})
+
+// app.delete('/disable-detection', (req, res) => {
+//     console.log('delete process')
+// })
 
 app.listen(PORT, () => console.log(`Server is live on ${PORT}`));
