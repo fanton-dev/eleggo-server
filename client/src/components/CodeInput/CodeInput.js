df7cc72c0af3a16f0376e2a59b99f031d8c1568e
Maddie02 <madlensarkisian@gmail.com>
2021-03-12 14:00:09 +0200

    [Feat] Refactored Code Input component References #3.

--- client/src/components/CodeInput/CodeInput.js
+++ client/src/components/CodeInput/CodeInput.js
@@ -1,43 +1,8 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
-
-function TabPanel(props) {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`vertical-tabpanel-${index}`}
-      aria-labelledby={`vertical-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box p={3}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
-}
-
-TabPanel.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.any.isRequired,
-  value: PropTypes.any.isRequired,
-};
-
-function a11yProps(index) {
-  return {
-    id: `vertical-tab-${index}`,
-    'aria-controls': `vertical-tabpanel-${index}`,
-  };
-}
+import styles from './CodeInput.module.css'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -49,41 +14,41 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+const TabPanel = ({ children, value, index }) => {
+  return (
+    <div>
+      {
+        value === index ? 
+          <textarea placeholder='Some code goes brrr...' className={styles.code}>
+          </textarea>
+        : ''
+      }
+    </div>
+  )
+}
+
 const CodeInput = () => {
   const classes = useStyles();
-  const [value, setValue] = React.useState(0);
+  const [selectedTab, setSelectedTab] = useState(0);
 
   const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
+    setSelectedTab(newValue);
+  }
 
   return (
     <div className={classes.root}>
-      <Tabs
-        orientation="horizontal"
-        value={value}
-        onChange={handleChange}
-        TabIndicatorProps={{ className: classes.indicator }}
-      >
-        <Tab label="Left" {...a11yProps(0)} />
-        <Tab label="None" {...a11yProps(1)} />
-        <Tab label="Right" {...a11yProps(2)} />
-        <Tab label="Prerequisite" {...a11yProps(3)} />
+      <Tabs value={selectedTab} onChange={handleChange} TabIndicatorProps={{ className: classes.indicator }} >
+          <Tab label="Left" />
+          <Tab label="None" />
+          <Tab label="Right" />
+          <Tab label="Prerequisite" />
       </Tabs>
-      <TabPanel value={value} index={0}>
-        Zdr
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-        None
-      </TabPanel>
-      <TabPanel value={value} index={2}>
-        Right
-      </TabPanel>
-      <TabPanel value={value} index={3}>
-        Prerequisite
-      </TabPanel>
+      <TabPanel value={selectedTab} index={0}></TabPanel>
+      <TabPanel value={selectedTab} index={1}></TabPanel>
+      <TabPanel value={selectedTab} index={2}></TabPanel>
+      <TabPanel value={selectedTab} index={3}></TabPanel>
     </div>
-  );
+  )
 }
 
 export default CodeInput;
