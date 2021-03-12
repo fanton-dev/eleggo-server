946154a2714d8f716705ef3f88de1cb2f43faa33
Maddie02 <madlensarkisian@gmail.com>
2021-03-12 10:08:27 +0200

    [Feat] Created mode switching component

---
+++ client/src/components/SwitchMode/SwitchMode.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ModeSelection from '../ModeSelection/ModeSelection'
+
+const SwitchMode = () => {
+    return (
+        <>
+            <ModeSelection />   
+        </>
+    )
+}
+
+export default SwitchMode
