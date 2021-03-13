8273162f2ae675e46a7df4ccfc0904ab985ee463
Maddie02 <madlensarkisian@gmail.com>
2021-03-13 17:05:12 +0200

    [Feat] Created delete request in server References #2.

--- server/index.js
+++ server/index.js
@@ -8,17 +8,21 @@ app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
+let startedProcess;
 
 app.post('/enable-detection', (req, res) => {
     console.log(req.body)
     const { mode, code: {left, none, right, prerequisite} } = req.body;
 
-    spawn('python', [`${mode}.py\', ${left}, ${none}, ${right}, ${prerequisite}`]);
+    startedProcess = spawn('python', [`${mode}.py\', ${left}, ${none}, ${right}, ${prerequisite}`]);
 })
 
 app.delete('/disable-detection', (req, res) => {
-    // kill process here
-    console.log('kill process')
+    if (startedProcess) {
+        console.log('kill process')
+        startedProcess.kill();
+        startedProcess = null;
+    }
 })
 
 app.listen(PORT, () => console.log(`Server is live on ${PORT}`));
