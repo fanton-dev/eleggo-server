3be58cd8436fb1ad1cb7f23cfa4cfb6255f4c1be
Maddie02 <madlensarkisian@gmail.com>
2021-03-13 11:58:00 +0200

    [Feat] Setup socket.io in server References #6.

--- server/index.js
+++ server/index.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const cors = require('cors');
 const { spawn } = require('child_process');
+const getData = require('./utils/getData')
 
 const app = express();
+const http = require('http').createServer(app)
+const PORT = process.env.PORT || 5000;
+
+const io = require('socket.io')(http, {
+  cors: {
+    origin: "http://localhost:3000"
+  }
+})
 
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT || 5000;
+io.on('connection', (socket) => {
+  setInterval(() => {
+    getData(socket)
+  }, 1000);
+});
 
 app.post('/enable-detection', (req, res) => {
     console.log(req.body)
@@ -20,4 +33,4 @@ app.post('/enable-detection', (req, res) => {
 //     console.log('delete process')
 // })
 
-app.listen(PORT, () => console.log(`Server is live on ${PORT}`));
+http.listen(PORT, () => console.log(`Server is live on ${PORT}`));
