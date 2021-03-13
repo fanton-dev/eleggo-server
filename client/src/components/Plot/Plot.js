b1b93ebd92c0bf39b08f8e4df83a591194d03e61
Maddie02 <madlensarkisian@gmail.com>
2021-03-13 18:09:41 +0200

    [Refactor] Removed unnecessary braces References #7.

--- client/src/components/Plot/Plot.js
+++ client/src/components/Plot/Plot.js
@@ -5,7 +5,7 @@ import io from 'socket.io-client'
 
 const socket = io('http://localhost:5000')
 
-const Plot = ({}) => {
+const Plot = () => {
   const [data, setData] = useState([])
 
   useEffect(() => {
