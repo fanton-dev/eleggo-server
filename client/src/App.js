8ee4955b0702816cd7bb37fa7363003d643b93bb
Maddie02 <madlensarkisian@gmail.com>
2021-03-13 11:55:34 +0200

    [Feat] Created plot component for real time eeg readings References #7.

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './components/Header/Header';
+import Plot from './components/Plot/Plot';
 import SwitchMode from './components/SwitchMode/SwitchMode';
 
 const App = () => {
   return (
     <div className="App">
       <Header />
+      <Plot />
       <SwitchMode />
     </div>
   );
---
+++ client/src/components/Plot/Plot.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { useEffect, useState } from "react";
+import { LineChart, Line, XAxis, YAxis } from "recharts";
+import io from 'socket.io-client'
+
+const socket = io('http://localhost:5000')
+
+const Plot = ({}) => {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    socket.on("eeg", response => {
+      setData(currentData => [...currentData, response]);
+    })
+  }, []);
+
+  console.log(data)
+  return (
+    <div>
+      <h1>Real Time CPU Usage</h1>
+      <LineChart width={500} height={300} data={data}>
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Line dataKey="value" />
+      </LineChart>
+    </div>
+  );
+};
+export default Plot
