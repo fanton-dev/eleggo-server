9c8ec9dcf18a313837e3a2a65445aac212ce5c8c
Angel Penchev <26301867+angel-penchev@users.noreply.github.com>
2021-09-16 18:27:49 +0300

    [Feat] Dev VPC config

---
+++ terraform/dev/vpc.tf
@@ -0,0 +1,17 @@
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "eleggo-dev-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-east-1a", "eu-east-1b"]
+  private_subnets = ["10.0.1.0/24"]
+  public_subnets  = ["10.0.101.0/24"]
+
+  enable_nat_gateway = true
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
