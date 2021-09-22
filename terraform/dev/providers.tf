a4e7cb85c598ca941cce41fef081757c25c03296
Angel Penchev <26301867+angel-penchev@users.noreply.github.com>
2021-09-22 22:00:58 +0300

    [Refactor] Seperated providers and their versions

---
+++ terraform/dev/providers.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.eks.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.eks.token
+}
