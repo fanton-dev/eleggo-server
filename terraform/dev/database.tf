3838bc5b9eaf039b7b4960ac0d2a7da3997c1b3a
Angel Penchev <26301867+angel-penchev@users.noreply.github.com>
2021-09-28 17:54:36 +0300

    [Feat] Dev database in AWS
    Provisioned a PostgreSQL database instance in AWS.

---
+++ terraform/dev/database.tf
@@ -0,0 +1,37 @@
+module "database" {
+  source = "terraform-aws-modules/rds/aws"
+
+  identifier = local.database_name
+
+  engine            = "postgres"
+  engine_version    = "13.3-R1"
+  instance_class    = "db.t2.micro"
+  allocated_storage = 5
+
+  name     = local.database_name
+  username = var.database_username
+  password = var.database_password
+  port     = "5432"
+
+  iam_database_authentication_enabled = true
+  vpc_security_group_ids              = [module.vpc.default_security_group_id]
+  subnet_ids                          = module.vpc.public_subnets
+  publicly_accessible                 = true
+
+  maintenance_window         = "Mon:00:00-Mon:03:00"
+  backup_window              = "03:00-06:00"
+  auto_minor_version_upgrade = true
+
+  monitoring_interval    = "30"
+  monitoring_role_name   = "database-monitor"
+  create_monitoring_role = true
+  deletion_protection    = false
+
+  tags = {
+    Environment = "dev"
+  }
+
+  depends_on = [
+    module.vpc
+  ]
+}
