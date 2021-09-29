156dd2fb5d68df9177a16a0659368746ad7bbffa
Angel Penchev <26301867+angel-penchev@users.noreply.github.com>
2021-09-30 00:57:48 +0300

    [Fix] Reduced number of NAT gateways
    I checked the billing tab...

--- terraform/main/vpc.tf
+++ terraform/main/vpc.tf
@@ -8,7 +8,9 @@ module "vpc" {
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
-  enable_nat_gateway = true
+  enable_nat_gateway     = true
+  single_nat_gateway     = true
+  one_nat_gateway_per_az = false
 
   manage_default_security_group = true
   default_security_group_egress = [{
