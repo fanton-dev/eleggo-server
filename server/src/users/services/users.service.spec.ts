36ea77ac7d27c8235eb0b020a6bed1745ecc0114
Angel Penchev <26301867+angel-penchev@users.noreply.github.com>
2021-10-18 20:08:46 +0300

    [Test] UsersService testing
    Added unit tests for the UsersService.
    References #17.

--- server/src/users/services/users.service.spec.ts
+++ server/src/users/services/users.service.spec.ts
@@ -1,12 +1,37 @@
+import { IUser, User } from '../entities/user.entity';
 import { Test, TestingModule } from '@nestjs/testing';
+
 import { UsersService } from './users.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { uuid } from 'uuidv4';
 
 describe('UsersService', () => {
   let service: UsersService;
 
+  const mockUser: IUser = {
+    id: uuid(),
+    username: 'gosho',
+    email: 'gosho@test.eleggo.app',
+    googleId: '133773578008135',
+    discordId: '8008135420889696',
+    githubId: '42080085',
+  };
+
+  const mockUsersRepository = {
+    findOne: jest.fn().mockImplementation(() => Promise.resolve(mockUser)),
+    save: jest
+      .fn()
+      .mockImplementation((entity: IUser) =>
+        Promise.resolve({ id: uuid(), ...entity }),
+      ),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        { provide: getRepositoryToken(User), useValue: mockUsersRepository },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
@@ -14,5 +39,33 @@ describe('UsersService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('findByUsername should search for a user by username and return it', async () => {
+    expect(await service.findByUsername(mockUser.username)).toEqual(mockUser);
+  });
+
+  it('findByEmail should search for a user by email and return it', async () => {
+    expect(await service.findByEmail(mockUser.email)).toEqual(mockUser);
+  });
+
+  it('findByGoogleId should search for a user by a google id and return it', async () => {
+    expect(await service.findByGoogleId(mockUser.googleId)).toEqual(mockUser);
+  });
+
+  it('findByDiscordId should search for a user by a discord id and return it', async () => {
+    expect(await service.findByDiscordId(mockUser.discordId)).toEqual(mockUser);
+  });
+
+  it('findByGithubId should search for a user by github id and return it', async () => {
+    expect(await service.findByGithubId(mockUser.githubId)).toEqual(mockUser);
+  });
+
+  it('createUser should create new users and return it', async () => {
+    expect(await service.createUser({ username: 'gosho' })).toEqual({
+      id: expect.any(String),
+      username: 'gosho',
+    });
+    expect(mockUsersRepository.save).toBeCalledTimes(1);
   });
 });
--- server/src/users/services/users.service.ts
+++ server/src/users/services/users.service.ts
@@ -5,29 +5,31 @@ import { IUser, User } from '../entities/user.entity';
 
 @Injectable()
 export class UsersService {
-  constructor(@InjectRepository(User) private userRepo: Repository<User>) {}
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) {}
 
   async findByUsername(username: string) {
-    return await this.userRepo.findOne({ username });
+    return await this.userRepository.findOne({ username });
   }
 
   async findByEmail(email: string) {
-    return await this.userRepo.findOne({ email });
+    return await this.userRepository.findOne({ email });
   }
 
   async findByGoogleId(googleId: string) {
-    return await this.userRepo.findOne({ googleId });
+    return await this.userRepository.findOne({ googleId });
   }
 
   async findByDiscordId(discordId: string) {
-    return await this.userRepo.findOne({ discordId });
+    return await this.userRepository.findOne({ discordId });
   }
 
   async findByGithubId(githubId: string) {
-    return await this.userRepo.findOne({ githubId });
+    return await this.userRepository.findOne({ githubId });
   }
 
   async createUser(userDetails: IUser) {
-    return await this.userRepo.save(userDetails);
+    return await this.userRepository.save(userDetails);
   }
 }
