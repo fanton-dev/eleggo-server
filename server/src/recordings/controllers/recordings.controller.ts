9468fa7052ced81e87c491e78667a2aad003dc4e
Angel Penchev <26301867+angel-penchev@users.noreply.github.com>
2021-10-20 11:23:32 +0300

    [Feat] Recordings list
    Added endpoint for listing a user's recordings.
    References #20.

--- server/src/recordings/controllers/recordings.controller.ts
+++ server/src/recordings/controllers/recordings.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import {
+  ApiCookieAuth,
+  ApiForbiddenResponse,
+  ApiOkResponse,
+} from '@nestjs/swagger';
+import { Request } from 'express';
+import { AuthSessionGuard } from 'src/auth/guards/auth.session.guard';
+import { RecordingsService } from '../services/recordings.service';
 
-@Controller('recordings')
-export class RecordingsController {}
+@Controller('/recordings')
+export class RecordingsController {
+  constructor(private readonly recordingsService: RecordingsService) {}
+
+  @Get('/')
+  @UseGuards(AuthSessionGuard)
+  @ApiCookieAuth()
+  @ApiOkResponse({ description: 'List of user recordings.' })
+  @ApiForbiddenResponse({ description: 'No user logon.' })
+  async getAll(@Req() req: Request) {
+    return this.recordingsService.listUserRecordings(req.user['username']);
+  }
+}
--- server/src/recordings/services/recordings.service.ts
+++ server/src/recordings/services/recordings.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { S3 } from 'aws-sdk';
+import { InjectAwsService } from 'nest-aws-sdk';
+import { configObject } from 'src/configuration';
 
 @Injectable()
-export class RecordingsService {}
+export class RecordingsService {
+  constructor(@InjectAwsService(S3) private readonly s3: S3) {}
+
+  async listUserRecordings(username: string) {
+    const queryResponse = await this.s3
+      .listObjectsV2({
+        Bucket: configObject.aws.recordingsS3Bucket,
+        Prefix: `${username}/`,
+      })
+      .promise();
+
+    const result = queryResponse.Contents.filter(
+      (item) => !item.Key.endsWith('/'),
+    ).map((item) => item.Key.replace(`${username}/`, ''));
+
+    return result;
+  }
+}
