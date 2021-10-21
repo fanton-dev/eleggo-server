bea383e8d56e5a47bd8f43ec0f9fad90b3a01f72
Angel Penchev <26301867+angel-penchev@users.noreply.github.com>
2021-10-21 17:42:36 +0300

    [Style] Grouped tests by method

--- server/src/users/services/users.service.spec.ts
+++ server/src/users/services/users.service.spec.ts
@@ -61,85 +61,109 @@ describe('UsersService', () => {
     expect(service).toBeDefined();
   });
 
-  it('findById should search for a user by id and return it', async () => {
-    expect(await service.findByUsername(mockUser.username)).toEqual({
-      ...mockUser,
-      password: hashedPassword,
+  describe('findById', () => {
+    it('should search for a user by id and return it', async () => {
+      expect(await service.findByUsername(mockUser.username)).toEqual({
+        ...mockUser,
+        password: hashedPassword,
+      });
     });
   });
 
-  it('findByUsername should search for a user by username and return it', async () => {
-    expect(await service.findByUsername(mockUser.username)).toEqual({
-      ...mockUser,
-      password: hashedPassword,
+  describe('findByUsername', () => {
+    it('should search for a user by username and return it', async () => {
+      expect(await service.findByUsername(mockUser.username)).toEqual({
+        ...mockUser,
+        password: hashedPassword,
+      });
     });
   });
 
-  it('findByEmail should search for a user by email and return it', async () => {
-    expect(await service.findByEmail(mockUser.email)).toEqual({
-      ...mockUser,
-      password: hashedPassword,
+  describe('findByEmail', () => {
+    it('should search for a user by email and return it', async () => {
+      expect(await service.findByEmail(mockUser.email)).toEqual({
+        ...mockUser,
+        password: hashedPassword,
+      });
     });
   });
 
-  it('findByGoogleId should search for a user by a google id and return it', async () => {
-    expect(await service.findByGoogleId(mockUser.googleId)).toEqual({
-      ...mockUser,
-      password: hashedPassword,
+  describe('findByGoogleId', () => {
+    it('should search for a user by a google id and return it', async () => {
+      expect(await service.findByGoogleId(mockUser.googleId)).toEqual({
+        ...mockUser,
+        password: hashedPassword,
+      });
     });
   });
 
-  it('findByDiscordId should search for a user by a discord id and return it', async () => {
-    expect(await service.findByDiscordId(mockUser.discordId)).toEqual({
-      ...mockUser,
-      password: hashedPassword,
+  describe('findByDiscordId', () => {
+    it('should search for a user by a discord id and return it', async () => {
+      expect(await service.findByDiscordId(mockUser.discordId)).toEqual({
+        ...mockUser,
+        password: hashedPassword,
+      });
     });
   });
 
-  it('findByGithubId should search for a user by github id and return it', async () => {
-    expect(await service.findByGithubId(mockUser.githubId)).toEqual({
-      ...mockUser,
-      password: hashedPassword,
+  describe('findByGithubId', () => {
+    it('should search for a user by github id and return it', async () => {
+      expect(await service.findByGithubId(mockUser.githubId)).toEqual({
+        ...mockUser,
+        password: hashedPassword,
+      });
     });
   });
 
-  it('createUser should create a new user, hash the password and return it', async () => {
-    const user = await service.createUser({
-      username: mockUser.username,
-      password: mockUser.password,
+  describe('createUser', () => {
+    it('should save a new user and return it', async () => {
+      const user = await service.createUser({
+        username: mockUser.username,
+        password: mockUser.password,
+      });
+
+      expect(user.id).toEqual(expect.any(String));
+      expect(user.username).toEqual(mockUser.username);
+      expect(user.password).toEqual(expect.any(String));
+
+      expect(mockUsersRepository.save).toBeCalledTimes(1);
     });
 
-    expect(user.id).toEqual(expect.any(String));
-    expect(user.username).toEqual(mockUser.username);
-    expect(user.password).toEqual(expect.any(String));
-    expect(user.password).not.toBe(mockUser.password);
+    it('should hash the user password', async () => {
+      const user = await service.createUser({
+        username: mockUser.username,
+        password: mockUser.password,
+      });
 
-    expect(mockUsersRepository.save).toBeCalledTimes(1);
-  });
-
-  it('createUser should create a code snippets storage for the user', async () => {
-    const user = await service.createUser({
-      username: mockUser.username,
-      password: mockUser.password,
+      expect(user.password).toEqual(expect.any(String));
+      expect(user.password).not.toBe(mockUser.password);
     });
 
-    expect(mockCodeSnippetsService.createUserStorage).toBeCalledTimes(1);
-    expect(mockCodeSnippetsService.createUserStorage).toBeCalledWith(
-      user.username,
-    );
+    it('should create a code snippets storage for the user', async () => {
+      const user = await service.createUser({
+        username: mockUser.username,
+        password: mockUser.password,
+      });
+
+      expect(mockCodeSnippetsService.createUserStorage).toBeCalledTimes(1);
+      expect(mockCodeSnippetsService.createUserStorage).toBeCalledWith(
+        user.username,
+      );
+    });
   });
 
-  it('verifyPassword should return false on wrong password', async () => {
-    const wrongPassword = 'wrong_password';
+  describe('validatePassword', () => {
+    it('should return false on wrong password', async () => {
+      const wrongPassword = 'wrong_password';
+      expect(
+        await service.verifyPassword(mockUser.username, wrongPassword),
+      ).toEqual(false);
+    });
 
-    expect(
-      await service.verifyPassword(mockUser.username, wrongPassword),
-    ).toEqual(false);
-  });
-
-  it('verifyPassword should return true on correct password', async () => {
-    expect(
-      await service.verifyPassword(mockUser.username, mockUser.password),
-    ).toEqual(true);
+    it('should return true on correct password', async () => {
+      expect(
+        await service.verifyPassword(mockUser.username, mockUser.password),
+      ).toEqual(true);
+    });
   });
 });
