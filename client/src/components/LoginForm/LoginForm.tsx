b22b7bdcb8263c8fffe8fba044e128fb03689485
Angel Penchev <26301867+angel-penchev@users.noreply.github.com>
2021-10-23 22:21:48 +0300

    [Feat] LoginForm validation using Formik
    Added field validation using formic.
    Verified login works correctly with the backend.
    References #21.

--- client/src/components/LoginForm/LoginForm.tsx
+++ client/src/components/LoginForm/LoginForm.tsx
@@ -1,3 +1,4 @@
+import * as yup from 'yup';
 import {
   Button,
   Container,
@@ -7,87 +8,93 @@ import {
   Typography,
 } from '@mui/material';
 import axios from 'axios';
-import React, { FC, useState } from 'react';
-
+import React, { FC, useContext } from 'react';
+import { useFormik } from 'formik';
+import { configurationContext } from '../../contexts/ConfigurationContext';
 import styles from './LoginForm.module.scss';
 
-interface LoginFormProps {
-  apiLoginEndpoint: string;
-}
+const LoginForm: FC<{}> = () => {
+  const apiRoot = useContext(configurationContext).api.root;
+  const apiLoginEndpoint = `${apiRoot}/auth/login`;
+  const validationSchema = yup.object({
+    username: yup.string().required('Username is required.'),
+    password: yup.string().required('Password is required.'),
+  });
 
-const LoginForm: FC<LoginFormProps> = (props: LoginFormProps) => {
-  const [username, setUsername] = useState<string>('');
-  const [password, setPassword] = useState<string>('');
-
-  const login = async () => {
-    const response = await axios.post(
-      props.apiLoginEndpoint,
-      {
-        username,
-        password,
-      },
-      {
+  const formik = useFormik({
+    initialValues: {
+      username: '',
+      password: '',
+    },
+    validationSchema: validationSchema,
+    onSubmit: async (values) => {
+      await axios.post(apiLoginEndpoint, values, {
         withCredentials: true,
-      },
-    );
+      });
+    },
+  });
 
-    if (response.data === 'success') {
-      return (window.location.href = '/');
-    }
-  };
-
-  const register = () => {
-    console.log('aaa');
+  const goToRegisterView = () => {
+    return true;
   };
 
   return (
     <Container className={styles.LoginForm} data-testid="LoginForm">
-      <Grid container spacing={2}>
-        <Grid item xs={12}>
-          <Typography variant="h4">Login</Typography>
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            label="Username"
-            variant="filled"
-            type="text"
-            required
-            fullWidth
-            onChange={(e: {
-              target: { value: React.SetStateAction<string> };
-            }) => setUsername(e.target.value)}
-          />
-        </Grid>
+      <form onSubmit={formik.handleSubmit}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <Typography variant="h4">Login</Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              required
+              fullWidth
+              id="username"
+              name="username"
+              label="Username"
+              type="text"
+              variant="filled"
+              value={formik.values.username}
+              onChange={formik.handleChange}
+              error={formik.touched.username && Boolean(formik.errors.username)}
+              helperText={formik.touched.username && formik.errors.username}
+            />
+          </Grid>
 
-        <Grid item xs={12}>
-          <TextField
-            label="Password"
-            variant="filled"
-            type="password"
-            required
-            fullWidth
-            onChange={(e: {
-              target: { value: React.SetStateAction<string> };
-            }) => setPassword(e.target.value)}
-          />
-        </Grid>
+          <Grid item xs={12}>
+            <TextField
+              required
+              fullWidth
+              id="password"
+              name="password"
+              label="Password"
+              type="password"
+              variant="filled"
+              value={formik.values.password}
+              onChange={formik.handleChange}
+              error={formik.touched.password && Boolean(formik.errors.password)}
+              helperText={formik.touched.password && formik.errors.password}
+            />
+          </Grid>
 
-        <Grid item xs={12}>
-          <Button
-            type="button"
-            variant="contained"
-            size="large"
-            fullWidth
-            onClick={() => login()}
-          >
-            Log in
-          </Button>
-          <Typography align="center">
-            Don't have an account?{' '}
-            <Link onClick={() => register()}>Register!</Link>
-          </Typography>
+          <Grid item xs={12}>
+            <Button
+              variant="contained"
+              size="large"
+              fullWidth
+              disabled={!(formik.isValid && formik.dirty)}
+              type="submit"
+            >
+              Log in
+            </Button>
+
+            <Typography align="center">
+              {"Don't have an account? "}
+              <Link onClick={() => goToRegisterView()}>Register!</Link>
+            </Typography>
+          </Grid>
         </Grid>
-      </Grid>
+      </form>
     </Container>
   );
 };
