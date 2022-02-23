7c56d2695c013b10fa0ac940c7ce83bc5734aca9
Angel Penchev <26301867+angel-penchev@users.noreply.github.com>
2022-02-23 18:32:37 +0200

    [Refactor] Minor resolution change

--- client/public/electron.js
+++ client/public/electron.js
@@ -11,8 +11,8 @@ let mainWindow;
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow({
-    width: 1024,
-    height: 800,
+    width: 1280,
+    height: 720,
     webPreferences: {
       experimentalFeatures: true,
       nodeIntegration: false, // is default value after Electron v5
